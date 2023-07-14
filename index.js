41dd574ff6557da8436ec1cc770630405a83807b
ThomasZRE <99052940+ThomasZRE@users.noreply.github.com>
2023-07-14 21:28:43 +0000

    Very simple game UI completed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-document.write("Hello, there!");
+let head = document.createElement("h1");
+head.innerText = "Welcome!\nThis is Rock Paper Scissors!";
+document.body.append(head);
+
+document.body.append("Which is your choice?");
 
 const array = ["Rock", "Paper", "Scissors"];
 
@@ -17,8 +21,8 @@ function playRound(pc, player) {
     else if (round.includes("Scissors") && round.includes("Rock")) {
         winner = round.indexOf("Rock");
     }
-    console.log("PC: " + pc);
-    console.log("You: " + player);
+    document.body.append(createP("PC: " + pc));
+    document.body.append(createP("You: " + player));
     return (winner ? "player" : "pc");
 }
 
@@ -30,45 +34,56 @@ function getComputerChoice() {
     return array[choice];
 }
 
-function game() {
-    let score = {"pc": 0, "player": 0};
-    
-    console.log("Round ");
-    let pc = getComputerChoice();
-    let player;         // Player's choice (ongoing)
-    // buttons for UI
-    const btn1 = document.createElement("button");
-    btn1.innerText = "Rock";
-
-    const btn2 = document.createElement("button");
-    btn2.innerText = "Paper";
-
-    const btn3 = document.createElement("button");
-    btn3.innerText = "Scissors";
-
-    document.body.append(btn1, btn2, btn3);
-
-    const buttons = document.querySelectorAll("button");
-        
-    buttons.forEach(button => button.addEventListener("click", function() {
-        player = button.innerText;
-    }));
-
+function game(player) {
+    pc = getComputerChoice();
     let winner = playRound(pc, player);
     score[winner]++;    
-    console.log(score["pc"]);
-    console.log(score["player"]);
-    winner === "Draw" ? console.log("Draw!") : console.log("The winner is " + winner);
-
-    console.log(`Total score\nPC: ${score.pc}\nYou: ${score.player}`);
-
-    if (score.pc === score.player) {
-        console.log("It's a Draw!");
-    } else if (score.pc > score.player) {
-        console.log("PC won!");
-    } else {
-        console.log("You won!");
-    }
+    document.body.append(createP(score["pc"]));
+    let focus = createP(score["player"]);
+    document.body.append(focus);
+    winner === "Draw" ? document.body.append(createP("Draw!\n")) : document.body.append(createP("The winner is " + winner + "\n"));
+    focus.scrollIntoView();
 }
 
-game();
+function createP(s) {
+    let el = document.createElement("p");
+    el.innerText = s;
+    return el;
+}
+
+let score = {"pc": 0, "player": 0};
+let pc;
+let player; 
+
+const btn1 = document.createElement("button");
+btn1.innerText = "Rock";
+
+const btn2 = document.createElement("button");
+btn2.innerText = "Paper";
+
+const btn3 = document.createElement("button");
+btn3.innerText = "Scissors";
+
+document.body.append(btn1, btn2, btn3);
+
+const buttons = document.querySelectorAll("button");
+    
+buttons.forEach(button => button.addEventListener("click", function() {
+    player = button.innerText;
+    game(player);
+
+    if (score.pc === 5 || score.player === 5) {
+        document.body.append("Total score") 
+        document.body.append(createP(`PC: ${score.pc}`));
+        document.body.append(createP(`You: ${score.player}`));
+    
+        if (score.pc === score.player) {
+            document.body.append("It's a Draw!");
+        } else if (score.pc > score.player) {
+            document.body.append("PC won!");
+        } else {
+            document.body.append("You won!");
+        }
+    }
+}));
+
